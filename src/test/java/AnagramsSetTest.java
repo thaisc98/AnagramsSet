80431536bb1731bf86a80471b6f63c62fb3804a1
thaisc98 <thaisc98@hotmail.es>
2021-05-18 21:06:46 -0400

    first commit and test

---
+++ src/test/java/AnagramsSetTest.java
@@ -0,0 +1,19 @@
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnagramsSetTest {
+
+
+    public static List<String> anagrams = new ArrayList<>();
+
+
+    @Test
+    public void check_Anagrams() {
+        boolean test = true;
+        Assert.assertEquals(test, true);
+    }
+
+}
